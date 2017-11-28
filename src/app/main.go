221c0b437151725f565abaf928c39c9193d2b5cd
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-11-29 00:04:03 +0900

    add gorm

--- src/app/main.go
+++ src/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -9,12 +11,46 @@ var (
 	Echo = echo.New()
 )
 
+/*
+ ORM library
+*/
+type Product struct {
+  gorm.Model
+  Code string
+  Price uint
+}
+func gormSample() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
+
+	// Delete - delete product
+	// db.Delete(&product)
+}
+
 func main() {
 	// Middleware
 	Echo.Use(middleware.Logger())
 	Echo.Use(middleware.Recover())
 	// routes
 	routes()
+	gormSample()
 	// start
 	Echo.Logger.Fatal(Echo.Start(":1323"))
 }
