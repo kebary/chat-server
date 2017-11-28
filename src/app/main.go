e184a9c2519dddbaf264e116aebc3d99211bfaa1
lc-kondo <kondo@lightcafe.co.jp>
2017-11-28 20:12:55 +0900

    authenticate

--- src/app/main.go
+++ src/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 var (
@@ -9,6 +10,11 @@ var (
 )
 
 func main() {
+	// Middleware
+	Echo.Use(middleware.Logger())
+	Echo.Use(middleware.Recover())
+  // routes
 	routes()
+  // start
 	Echo.Logger.Fatal(Echo.Start(":1323"))
 }
--- src/app/routes.go
+++ src/app/routes.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"github.com/labstack/echo"
 	"net/http"
+	"time"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
 )
+
+func authenticate(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	if username == "test" && password == "test" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "test"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("UNnx5PbFtH7b8HKazP"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+	return echo.ErrUnauthorized
+}
 
 func routes() {
   Echo.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	Echo.POST("/login", authenticate)
 }
