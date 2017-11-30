ca71b4e5ff498d7d0d4c77023aa70ad8478bdc51
tsuyukusa-rk <rk.1227.89@gmail.com>
2017-11-30 20:28:05 +0900

    web socket test

--- src/app/controllers/api/v1/talk/index.go
+++ src/app/controllers/api/v1/talk/index.go
@@ -5,12 +5,18 @@ import (
 	"app/db/scheme"
 	"github.com/labstack/echo"
 	"net/http"
+	"fmt"
+	"github.com/gorilla/websocket"
 )
 
 type (
 	params struct {
 	  Msg string `json:"msg"`
 	}
+)
+
+var (
+	upgrader = websocket.Upgrader{}
 )
 
 func Get(c echo.Context) error {
@@ -29,4 +35,27 @@ func Post(c echo.Context) (err error) {
 	db.Database.Create(&talk)
 	var res interface{}
 	return c.JSON(http.StatusOK, res)
+}
+
+func Ws(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
 }
--- src/app/routes/index.go
+++ src/app/routes/index.go
@@ -2,14 +2,16 @@ package routes
 
 import (
 	"app/controllers/api/authenticate"
-	"app/controllers/root"
+	// "app/controllers/root"
 	"app/controllers/api/v1/talk"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func Routes(Echo *echo.Echo) {
-	Echo.GET("/", root.Get)
+	Echo.Static("/", "public")
+	// Echo.GET("/", root.Get)
+	Echo.GET("/ws", talk.Ws)
 	Echo.POST("/api/authenticate", authenticate.Post)
 	apis := Echo.Group("/api/v1")
 	apis.Use(middleware.JWT([]byte("UNnx5PbFtH7b8HKazP")))
